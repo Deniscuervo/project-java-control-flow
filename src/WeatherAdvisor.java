8e1570af812f437a3ed8bf50ac92c0bdca974f6d
Denis Cuervo <dlisethdaza@gmail.com>
2025-11-01 10:50:11 -0500

    Desafio FizzBuzz

--- src/WeatherAdvisor.java
+++ src/WeatherAdvisor.java
@@ -1,16 +1,23 @@
 public class WeatherAdvisor {
     public static void main(String[] args) {
-        int temperature = 5; // Temperatura en grados Celsius
-
-        System.out.println("The temperature is " + temperature + "°C.");
-
-if (temperature > 30) {
-    System.out.println("It's a hot day. Remember to stay hydrated!");
-} else if (temperature > 20) {
-    System.out.println("It's a nice warm day. Enjoy the weather!");
-} else if (temperature > 10) {
-    System.out.println("It's a bit cool. You might need a light jacket.");
-} else {
-    System.out.println("It's cold. Wear warm clothes!");
-}    }
+        //  números del 1 al 100
+        for (int i = 1; i <= 100; i++) {
+            // Verificamos si el número es múltiplo de 3 y 5
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            // Si solo es múltiplo de 3
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            // Si solo es múltiplo de 5
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            // Si no cumple ninguna condición anterior
+            else {
+                System.out.println(i);
+            }
+        }
+    }
 }
